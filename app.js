ca4453c87a8d8ba25a1ece93d54d975541f8954f
Anna Ulanova <anetteua0@gmail.com>
2017-07-20 17:06:55 -0700

    m

--- app.js
+++ app.js
@@ -3,23 +3,24 @@ const http = require('http');
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer(function(req, res) {
-		res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
 
-		const config = JSON.parse(process.env.APP_CONFIG);
-		const MongoClient = require('mongodb').MongoClient;
-		const mongoPassword = 'your_password_here';
+  const config = JSON.parse(process.env.APP_CONFIG);
+  const MongoClient = require('mongodb').MongoClient;
+  const mongoPassword = 'your_password_here';
 
-		MongoClient.connect(
-			"mongodb://" + config.mongo.user,
-      + ":" + mongoPassword + "@" +
-		config.mongo.hostString,
-			function(err, db) {
-						if(!err) {
-								res.end("We are connected to MongoDB\n");
-						} else {
-								res.end("Error while connecting to MongoDB\n");
-						}
-		});
+  MongoClient.connect(
+    "mongodb://" + config.mongo.user
+    + ":" + mongoPassword + "@" +
+    config.mongo.hostString,
+    function(err, db) {
+      if(!err) {
+        res.end("We are connected to MongoDB\n");
+      } else {
+        res.end("Error while connecting to MongoDB\n");
+      }
+    }
+  );
 });
 server.listen(PORT, '0.0.0.0', function(err) {
   console.log(`server up: ${PORT}`)
